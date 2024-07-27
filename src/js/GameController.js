0b8ccc71f1307d2e6dd23f8eb4963e14ae57a250
Mikhail Brann <alunos13@yandex.com>
2024-07-27 18:37:57 +0300

    main +chapter info func

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -16,30 +16,32 @@ export default class GameController {
   }
 
   init() {
+    this.charactersOnField = [];
+
     // TODO: add event listeners to gamePlay events
+    this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
+    this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
+
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
     const heroesTeam = generateTeam([Bowman, Swordsman, Magician], 3, 3);
     const enemiesTeam = generateTeam([Daemon, Undead, Vampire], 3, 3);
     const startHeroesPositionPointExludes = [];
     const startEnemiesPositionPointExludes = [];
-    const startChaptersPositions = [];
     
-
     heroesTeam.characters.forEach( character => {
       const position = calcPositionToField(this.gamePlay.boardSize, startHeroesPositionPointExludes, 'left');
       startHeroesPositionPointExludes.push(position);
-      startChaptersPositions.push(new PositionedCharacter(character.value, position));
+      this.charactersOnField.push(new PositionedCharacter(character.value, position));
     });
-
 
     enemiesTeam.characters.forEach( character => {
       const position = calcPositionToField(this.gamePlay.boardSize, startEnemiesPositionPointExludes, 'right');
       startEnemiesPositionPointExludes.push(position);
-      startChaptersPositions.push(new PositionedCharacter(character.value, position));
+      this.charactersOnField.push(new PositionedCharacter(character.value, position));
     });
 
-    this.gamePlay.redrawPositions(startChaptersPositions);
+    this.gamePlay.redrawPositions(this.charactersOnField);
   }
 
   onCellClick(index) {
@@ -48,6 +50,20 @@ export default class GameController {
 
   onCellEnter(index) {
     // TODO: react to mouse enter
+    const findChar = this.charactersOnField.find(char => char.position === index);
+
+    if(findChar) {
+      const characterRenderInfo = (strings, ...values) =>
+        strings.reduce((acc, str, i) => {
+          return `${acc}${str}${values[i] || ''}`;
+        }, '');
+      const { level, attack, defence, health } = findChar.character;
+
+      this.gamePlay.showCellTooltip(
+        characterRenderInfo`🎖${level} ⚔${attack} 🛡${defence} ❤${health}`, 
+        index
+      );
+    }
   }
 
   onCellLeave(index) {
